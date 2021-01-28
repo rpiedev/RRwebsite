0852665361b3914a48863fe6be16c7c0c3abd6a1
redassser <redryan890@gmail.com>
2021-01-28 11:39:13 -0500

    h

--- public/script.js
+++ public/script.js
@@ -1,11 +1,11 @@
 const players = document.getElementById("players");
 const temps = document.getElementById("temps");
-const data = document.getElementById("data");
+const cpudata = document.getElementById("data");
 
 fetch("/data")
 .then(response => response.json())
 .then(data => {
     players.innerText = data.players;
     temps.innerText = data.temps;
-    data.innerText = data.data;
+    cpudata.innerText = data.data;
 })
