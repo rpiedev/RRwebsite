915fc8bbae5f4b3874aece8b41ae93949fbe9f32
redassser <redryan890@gmail.com>
2021-01-27 20:33:57 -0500

    try again

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const { exec } = require("child_process");
 const app = express();
 
 const magic = `screen -r Sans-SMP -p 0 -X stuff "list $(printf '\\r')"`;
-console.log(magic);
-/*exec(magic, (error, stdout, stderr) => {
+exec(magic, (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
@@ -15,4 +14,3 @@ console.log(magic);
     }
     console.log(stdout)
 })
-*/
