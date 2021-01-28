3a835ec5524ee66889ee0f7bb576cee9242f8aa6
redassser <redryan890@gmail.com>
2021-01-27 21:23:42 -0500

    might work?

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const { exec } = require("child_process");
 const app = express();
-
 const magic = `screen -r Sans-SMP -p 0 -X stuff "list $(printf '\\r')"`;
-console.log(magic)
+
+app.use(express.static("public"));
+app.get("/", (request, response) => {
+    response.sendFile(__dirname + "/views/index.html");
+});
+
+const data = {
+    players: ["GMiningHero","Mikorod622"],
+    performance: "data"
+  }
 exec(magic, (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(stdout)
+    app.get("/data", (request, response) => {
+        response.json(data);
+    });
 })
+
+const listener = app.listen(process.env.PORT, () => {
+    console.log("Your app is listening on port " + listener.address().port);
+});
